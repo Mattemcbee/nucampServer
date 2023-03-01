784c9bc314c06d682d263cfb542d4595628c9a6d
Matt McBee <mattemcbee@gmail.com>
2023-02-28 20:26:58 -0500

    change to token based

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const partnerRouter = express.Router();
+const authenticate = require('../authenticate')
+
 const Partner = require('../models/partner')
 
 partnerRouter.route('/')
@@ -12,7 +14,7 @@ partnerRouter.route('/')
             .catch(err => next(err))
     })
 
-    .post((req, res, next) => {
+    .post(authenticate.verifyUser,(req, res, next) => {
         Partner.create(req.body)
             .then((partner) => {
                 res.status(200).json(partner)
@@ -20,12 +22,12 @@ partnerRouter.route('/')
             .catch(err => next(err))
     })
 
-    .put((req, res) => {
+    .put(authenticate.verifyUser,(req, res) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /partners');
     })
 
-    .delete((req, res, next) => {
+    .delete(authenticate.verifyUser,(req, res, next) => {
         Partner.deleteMany()
             .then((partners) => {
                 res.status(200).json(partners)
@@ -44,12 +46,12 @@ partnerRouter.route('/:partnerId')
             .catch(err => next(err))
     })
 
-    .post((req, res) => {
+    .post(authenticate.verifyUser,(req, res) => {
         res.statusCode = 403;
         res.end('post not supported')
     })
 
-    .put((req, res, next) => {
+    .put(authenticate.verifyUser,(req, res, next) => {
         Partner.findByIdAndUpdate(req.params.partnerId, {
             $set: req.body
         }, { new: true })
@@ -59,7 +61,7 @@ partnerRouter.route('/:partnerId')
             .catch(err => next(err))
     })
 
-    .delete((req, res, next) => {
+    .delete(authenticate.verifyUser,(req, res, next) => {
         Partner.findByIdAndDelete(req.params.partnerId)
             .then((partner) => {
                 res.status(200).json(partner)
