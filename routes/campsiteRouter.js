8104b96800d139655a431b1036331b35074507a8
Matt McBee <mattemcbee@gmail.com>
2023-02-28 20:59:42 -0500

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -5,16 +5,17 @@ const authenticate = require('../authenticate')
 const campsiteRouter = express.Router();
 
 campsiteRouter.route('/')
-
-    .get((req, res, next) => {
-        Campsite.find()
-            .then(campsites => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsites)
-            })
-            .catch(err => next(err))
+.get((req, res, next) => {
+    Campsite.find()
+    .populate('comments.author')
+    .then(campsites => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsites);
     })
+    .catch(err => next(err));
+})
+
     .post(authenticate.verifyUser,(req, res, next) => {
         Campsite.create(req.body)
             .then(campsite => {
@@ -43,18 +44,18 @@ campsiteRouter.route('/')
     });
 
 
-campsiteRouter.route('/:campsiteId')
-
+    campsiteRouter.route('/:campsiteId')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json')
-                res.json(campsite)
-            })
-            .catch(err => next(err))
+        .populate('comments.author')
+        .then(campsite => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(campsite);
+        })
+        .catch(err => next(err));
     })
-
+    
     .post(authenticate.verifyUser,(req, res) => {
         res.statusCode = 403;
         res.end('post not supported')
@@ -82,27 +83,30 @@ campsiteRouter.route('/:campsiteId')
             .catch(err => next(err))
     })
 
-campsiteRouter.route('/:campsiteId/comments')
+    campsiteRouter.route('/:campsiteId/comments')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
-            .then(campsite => {
-                if (campsite) {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(campsite.comments);
-                } else {
-                    err = new Error(`Campsite ${req.params.campsiteId} not found`);
-                    err.status = 404;
-                    return next(err);
-                }
-            })
-            .catch(err => next(err));
+        .populate('comments.author')
+        .then(campsite => {
+            if (campsite) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(campsite.comments);
+            } else {
+                err = new Error(`Campsite ${req.params.campsiteId} not found`);
+                err.status = 404;
+                return next(err);
+            }
+        })
+        .catch(err => next(err));
     })
-    .post(authenticate.verifyUser,(req, res, next) => {
+    .post(authenticate.verifyUser, (req, res, next) => {
         Campsite.findById(req.params.campsiteId)
-            .then(campsite => {
-                if (campsite) {
-                    campsite.comments.push(req.body);
+        .then(campsite => {
+            if (campsite) {
+                req.body.author = req.user._id;
+                campsite.comments.push(req.body);
+    
                     campsite.save()
                         .then(campsite => {
                             res.statusCode = 200;
@@ -145,10 +149,12 @@ campsiteRouter.route('/:campsiteId/comments')
             .catch(err => next(err));
     });
 
-campsiteRouter.route('/:campsiteId/comments/:commentId')
+    campsiteRouter.route('/:campsiteId/comments/:commentId')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
-            .then(campsite => {
+        .populate('comments.author')
+        .then(campsite => {
+    
                 if (campsite && campsite.comments.id(req.params.commentId)) {
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
